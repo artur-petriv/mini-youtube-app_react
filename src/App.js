d011b2003ae34b93ed61bc8fc32171cfad1bb370
Artur Petriv <artur.petriv@gmail.com>
2020-09-14 00:56:59 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import RecommendedVideos from "./RecommendedVideos";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="app_page">
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./RecommendedVideos.css";
+
+function RecomendedVideos() {
+  return (
+    <div className="recommendedVideos">
+      <h2>RecommendedVideos</h2>
+    </div>
+  );
+}
+
+export default RecomendedVideos;
