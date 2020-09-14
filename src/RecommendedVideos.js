3b82fa3c71c69f90794b2f1884718c25bf4c4684
Artur Petriv <artur.petriv@gmail.com>
2020-09-14 22:34:39 +0300

    Add videoCards, Search page...

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,45 @@
 import React from "react";
 import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
 
 function RecomendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h2>RecommendedVideos</h2>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Защита информации в сети / Белые хакеры"
+          views="189K views"
+          timestamp="8 months ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GiszfMNpU0-SdpHUg6ln31JXkpzxe0BXDpnhw=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="АйТиБорода"
+          image="https://i.ytimg.com/vi/4oLzvhDRZT4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA7otNukUm_kAC9EPbk30q7DIgr5A"
+        />
+        <VideoCard
+          title="Защита информации в сети / Белые хакеры"
+          views="189K views"
+          timestamp="8 months ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GiszfMNpU0-SdpHUg6ln31JXkpzxe0BXDpnhw=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="АйТиБорода"
+          image="https://i.ytimg.com/vi/4oLzvhDRZT4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA7otNukUm_kAC9EPbk30q7DIgr5A"
+        />
+        <VideoCard
+          title="Защита информации в сети / Белые хакеры"
+          views="189K views"
+          timestamp="8 months ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GiszfMNpU0-SdpHUg6ln31JXkpzxe0BXDpnhw=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="АйТиБорода"
+          image="https://i.ytimg.com/vi/4oLzvhDRZT4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA7otNukUm_kAC9EPbk30q7DIgr5A"
+        />
+        <VideoCard
+          title="Защита информации в сети / Белые хакеры"
+          views="189K views"
+          timestamp="8 months ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GiszfMNpU0-SdpHUg6ln31JXkpzxe0BXDpnhw=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="АйТиБорода"
+          image="https://i.ytimg.com/vi/4oLzvhDRZT4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA7otNukUm_kAC9EPbk30q7DIgr5A"
+        />
+      </div>
     </div>
   );
 }
