e891ea40a697a367423f8ba8f123d1aa8f5eef53
Artur Petriv <artur.petriv@gmail.com>
2020-09-15 13:16:07 +0300

    Channel Row, Video Row

--- src/ChannelRow.js
+++ src/ChannelRow.js
@@ -1,6 +1,7 @@
 import React from "react";
-import "./ChannelRow.css";
 import { Avatar } from "@material-ui/core";
+import VerifiedIcon from "@material-ui/icons/CheckCircle";
+import "./ChannelRow.css";
 
 function ChannelRow({
   image,
@@ -13,7 +14,16 @@ function ChannelRow({
 }) {
   return (
     <div className="channelRow">
-      <Avatar className="channelRow__logo" alt={channel} />
+      <Avatar className="channelRow__logo" alt={channel} src={image} />
+      <div className="channelRow__text">
+        <h4>
+          {channel} {verified && <VerifiedIcon />}
+        </h4>
+        <p>
+          {subs} subscribers • {noOfVideos} videos
+        </p>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SearchPage.css";
 import ChannelRow from "./ChannelRow";
+import VideoRow from "./VideoRow";
 import TuneOutlinedIcon from "@material-ui/icons/TuneOutlined";
 
 function SearchPage() {
@@ -13,12 +14,24 @@ function SearchPage() {
       <hr />
 
       <ChannelRow
-        image="https://i.ytimg.com/vi/HNooPVy2kUs/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB-ygXxEwpwanMCjaiHMFQPV_Mi9w"
+        image="https://yt3.ggpht.com/a/AATXAJwnl6a50TYz7UpGD2rGtXdmO9B2V4Oik3BcFWwd=s176-c-k-c0x00ffffff-no-rj-mo"
         channel="Best PC"
         verified
         subs="521K"
         noOfVideos={382}
         description="Alienware Aurora R9 - Great Overall Gaming"
+      />
+      <hr />
+
+      <VideoRow
+        title="Защита информации в сети / Белые хакеры"
+        views="189K"
+        timestamp="2 months ago"
+        subs="243K"
+        channelImage="https://yt3.ggpht.com/a-/AOh14GiszfMNpU0-SdpHUg6ln31JXkpzxe0BXDpnhw=s68-c-k-c0x00ffffff-no-rj-mo"
+        channel="АйТиБорода"
+        image="https://i.ytimg.com/vi/4oLzvhDRZT4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA7otNukUm_kAC9EPbk30q7DIgr5A"
+        description="Это необычный выпуск. В нем я постарался приоткрыть аспекты жизни и работы специалиста по уязвимостям и ..."
       />
     </div>
   );
